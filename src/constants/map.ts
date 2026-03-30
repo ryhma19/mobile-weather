60cfb12f6a455a50d438cf87e2f7d114b4aebd83
Aapo Ronkainen <t3roaa00@students.oamk.fi>
2026-03-30 14:58:44 +0300

    kartta näyttää enemmän maastonmuotoja jne.

--- src/constants/map.ts
+++ src/constants/map.ts
@@ -1,7 +1,7 @@
 export const MML_API_KEY = process.env.EXPO_PUBLIC_MML_API_KEY ?? ''
 
 export const MML_BACKGROUND_TILE_URL =
-  `https://avoin-karttakuva.maanmittauslaitos.fi/avoin/wmts/1.0.0/taustakartta/default/WGS84_Pseudo-Mercator/{z}/{y}/{x}.png?api-key=${MML_API_KEY}`
+  `https://avoin-karttakuva.maanmittauslaitos.fi/avoin/wmts/1.0.0/maastokartta/default/WGS84_Pseudo-Mercator/{z}/{y}/{x}.png?api-key=${MML_API_KEY}`
 
 export const INITIAL_REGION = {
   latitude: 60.1699,
